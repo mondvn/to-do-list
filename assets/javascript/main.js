7b525b7e024a43e9cbc39c5c4f47e850df22dcf2
MondVn <trannamanh94@gmail.com>
2022-12-09 01:32:20 +0700

    fix update task

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -9,8 +9,8 @@ let formInput = $('.form-input')
 let listTask = $('.list-tasks')
 
 renderTasks(getTasksFromLocalStorage())
-console.log(getTasksFromLocalStorage())
 
+// Lắng nghe các sự kiện
 btnAddTask.onclick = () => {
   if(!formInput.value) {
     alert('Vui lòng nhập thông tin task')
@@ -21,14 +21,13 @@ btnAddTask.onclick = () => {
   let taskId = btnAddTask.getAttribute('id')
 
   if (taskId === 0 || taskId) {
-    tasks[taskId] = {content: formInput.value}
+    tasks[taskId].content = formInput.value
     btnAddTask.removeAttribute('id')
-    resetUI()
   } else {
     tasks.push({content: formInput.value, childTasks: []})
   }
-  formInput.value = ''
 
+  resetUI()
   localStorage.setItem('tasks', JSON.stringify(tasks))
   renderTasks(tasks)
 }
@@ -45,7 +44,6 @@ btnUpdateChildTask.onclick = () => {
   let tasks = getTasksFromLocalStorage()
   let id = btnUpdateChildTask.getAttribute('id')
   arrayId = id.split(':')
-  // console.log(tasks[arrayId[0]].childTasks[arrayId[1]].childContent)
   tasks[arrayId[0]].childTasks[arrayId[1]] = ({childContent: formInput.value})
   btnUpdateChildTask.removeAttribute('id')
 
@@ -63,6 +61,56 @@ btnAddChildTask.onclick = () => {
   resetUI()
   localStorage.setItem('tasks', JSON.stringify(tasks))
   renderTasks(tasks)
+}
+
+// Handle xử lý
+function editTask(id) {
+  resetUI()
+  let tasks = getTasksFromLocalStorage()
+  formInput.value = tasks[id].content
+  btnAddTask.innerHTML = 'Sửa task'
+  btnAddTask.classList.add('update')
+  btnAddTask.setAttribute('id', id)
+  btnCancelUpdate.classList.remove('disabled')
+}
+
+function deleteTask(id) {
+  if(confirm('Bạn có muốn xóa không?')) {
+    let tasks = getTasksFromLocalStorage()
+    tasks.splice(id, 1)
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+    renderTasks(tasks)
+  }
+}
+
+function addChildTask(id) {
+  resetUI()
+  btnAddTask.classList.add('disabled')
+  btnAddChildTask.classList.remove('disabled')
+  btnCancelUpdate.classList.remove('disabled')
+  btnAddChildTask.setAttribute('id', id)
+}
+
+function editChildTask(id) {
+  resetUI()
+  let arrayId = id.split(':')
+  let tasks = getTasksFromLocalStorage()
+  formInput.value = tasks[arrayId[0]].childTasks[arrayId[1]].childContent
+  btnUpdateChildTask.setAttribute('id', id)
+  btnAddTask.classList.add('disabled')
+  btnUpdateChildTask.classList.remove('disabled')
+  btnCancelUpdate.classList.remove('disabled')
+}
+
+function deleteChildTask(id) {
+  if(confirm('Bạn có muốn xóa chứ?')) {
+    let tasks = getTasksFromLocalStorage()
+    let arrayId = id.split(':')
+    tasks[arrayId[0]].childTasks.splice(arrayId[1], 1)
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+    renderTasks(tasks)
+  }
+
 }
 
 function resetUI() {
@@ -105,55 +153,7 @@ function renderTasks(tasks = []) {
   listTask.innerHTML = htmls
 }
 
-function addChildTask(id) {
-  resetUI()
-  btnAddTask.classList.add('disabled')
-  btnAddChildTask.classList.remove('disabled')
-  btnCancelUpdate.classList.remove('disabled')
-  btnAddChildTask.setAttribute('id', id)
-}
-
-function editTask(id) {
-  resetUI()
-  let tasks = getTasksFromLocalStorage()
-  formInput.value = tasks[id].content
-  btnAddTask.innerHTML = 'Sửa task'
-  btnAddTask.classList.add('update')
-  btnAddTask.setAttribute('id', id)
-  btnCancelUpdate.classList.remove('disabled')
-}
-
-function editChildTask(id) {
-  resetUI()
-  let arrayId = id.split(':')
-  let tasks = getTasksFromLocalStorage()
-  formInput.value = tasks[arrayId[0]].childTasks[arrayId[1]].childContent
-  btnUpdateChildTask.setAttribute('id', id)
-  btnAddTask.classList.add('disabled')
-  btnUpdateChildTask.classList.remove('disabled')
-  btnCancelUpdate.classList.remove('disabled')
-}
-
-function deleteChildTask(id) {
-  if(confirm('Bạn có muốn xóa chứ?')) {
-    let tasks = getTasksFromLocalStorage()
-    let arrayId = id.split(':')
-    tasks[arrayId[0]].childTasks.splice(arrayId[1], 1)
-    localStorage.setItem('tasks', JSON.stringify(tasks))
-    renderTasks(tasks)
-  }
-
-}
-
-function deleteTask(id) {
-  if(confirm('Bạn có muốn xóa không?')) {
-    let tasks = getTasksFromLocalStorage()
-    tasks.splice(id, 1)
-    localStorage.setItem('tasks', JSON.stringify(tasks))
-    renderTasks(tasks)
-  }
-}
-
 function getTasksFromLocalStorage() {
   return localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : []
 }
+
